ac5f026395c4808af1f5d59ae43050ef7e4ca0b1
Matt Vincent <matt@art209.net>
2013-03-07 00:20:14 -0800

    Foldy
    Responsive WP child theme for Toolbox

---
+++ full-width-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Full-width, no sidebar
+ * Description: A full-width template with no sidebar
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+
+get_header(); ?>
+<script></script>
+<div class="grid-6">
+		<div id="primary" class="full-width">
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+</div><!-- .grid-6 -->
+<div class="clear"></div>
+<div class="grid-6">
+<?php get_footer(); ?>
---
+++ secondarypage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template used for displaying secondary page header
+ * Description: New header stuff
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+?>
+<div id="secondarypage-header">
+</div>
+	
